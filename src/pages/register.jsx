5934411da089317941ba2dc4e4426cc09d29cd94
João Araújo <a23282@alunos.ipca.pt>
2024-05-06 23:06:34 +0100

    Chore: Criei a página register

---
+++ src/pages/register.jsx
@@ -0,0 +1,15 @@
+import Layout from "../components/layout";
+import "./styles.css";
+import ImgSideInputs from "../components/imgSideInputs";
+import React from "react";
+function Register() {
+  return (
+    <Layout>
+      <>
+        <ImgSideInputs></ImgSideInputs>
+      </>
+    </Layout>
+  );
+}
+
+export default Register;
