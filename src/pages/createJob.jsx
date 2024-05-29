f11bf7b0f47453181073220273ae82820f186c22
João Peixoto <joaopeixoto@grupojpm.pt>
2024-05-29 11:04:44 +0100

    [add] -> login (NavBar tambem é afetada) [add] -> CreateJoboffer (POST)

--- src/pages/createJob.jsx
+++ src/pages/createJob.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/layout";
+import { createJobOffer } from "../shared/apiService";
 import SelectionProcess from "../components/selectionProcess";
 
 function CreateJob() {
+  const [titulo, SetTitulo] = useState("");
+  const [descricao, SetDescricao] = useState("");
+  const [requirimentos, SetRequirimentos] = useState("");
+  const [beneficios, SetBeneficios] = useState("");
+
+  const postTitulo = (event) => {
+    SetTitulo(event.target.value);
+  };
+  const postDescricao = (event) => {
+    SetDescricao(event.target.value);
+  };
+  const postRequirementos = (event) => {
+    SetRequirimentos(event.target.value);
+  };
+  const postBeneficios = (event) => {
+    SetBeneficios(event.target.value);
+  };
+
+  const Create = () => {
+    const obj = {
+      titulo: titulo,
+      descricao: descricao,
+      requirimentos: requirimentos,
+      beneficios: beneficios,
+    };
+    createJobOffer(obj);
+  };
   return (
     <>
       <Layout>
@@ -16,22 +44,25 @@ function CreateJob() {
               type="text"
               placeholder="Digite algo..."
               className="input-box"
+              onChange={postTitulo}
             />
           </div>
           <div className="input-container">
             <h3 className="title title-medium text-left">Descrição</h3>
             <textarea
               placeholder="Digite algo..."
-              class="input-box"
+              className="input-box"
               rows="1"
+              onChange={postDescricao}
             ></textarea>
           </div>
           <div className="input-container">
             <h3 className="title title-medium text-left">Requerimentos</h3>
             <textarea
               placeholder="Digite algo..."
-              class="input-box"
+              className="input-box"
               rows="1"
+              onChange={postRequirementos}
             ></textarea>
           </div>
           <div className="input-container">
@@ -40,11 +71,14 @@ function CreateJob() {
               placeholder="Digite algo... "
               class="input-box"
               rows="1"
+              onChange={postBeneficios}
             ></textarea>
           </div>
           <SelectionProcess />
           <div className="d-flex justify-content-center">
-            <button className="create-button">Create</button>
+            <button className="create-button" onClick={Create}>
+              Create
+            </button>
           </div>
         </div>
       </Layout>
